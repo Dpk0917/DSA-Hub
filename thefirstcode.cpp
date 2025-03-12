1e2c2558bf7d1e6215921e79b665e1e7c5d2c369
DEEPAK <dpk0917@gmail.com>
2025-03-13 02:10:32 +0530

    Added thefirstcode.cpp

---
+++ thefirstcode.cpp
@@ -0,0 +1,90 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout<<"Namaste duniya\n";
+    
+    int a = 8 ;
+    cout<< a << endl;
+    
+    char b = 'k' ;
+    cout<< b << endl;
+    
+    bool c = 1 ;
+    cout<< c << endl;
+    
+    bool d = false ;
+    cout<< d << endl;
+    
+    float e = 1.8 ;
+    cout<< e << endl;
+    
+    double f = 9.88 ;
+    cout<< f << endl; 
+    
+    int size = sizeof(a);
+    cout << "size of a is " << size << endl;
+    
+    bool mysize = sizeof(d);
+    cout << "size of d is " << mysize << endl;
+
+    int g = 'g' ;
+    cout << g << endl;
+    
+    char ch =  104 ;
+    cout<< ch << endl;
+    
+    unsigned int i = 112;
+    cout<< i << endl;
+    
+    unsigned int j = -112;
+    cout<< j << endl;
+    
+    int k = 2/5;
+    cout<< k << endl;
+    
+    int l = 2.0/5;           
+    cout<< l << endl;
+    cout<< 2.0/5 << endl;
+    
+    double m = 2.0/5;           
+    cout<< m << endl;
+    
+    int n = 5;
+    int o = 9;
+    
+    bool first (n==o);
+    cout<< first << endl;
+    
+    bool second (n<o);
+    cout<< second << endl;
+    
+    bool third (n>o);
+    cout<< third << endl;
+    
+    bool fourth (n>=o);
+    cout<< fourth << endl;
+    
+    bool fifth (n<=o);
+    cout<< fifth << endl;
+    
+    bool sixth (n!=o);
+    cout<< sixth << endl;
+    
+    int p = 24;
+    cout<< !p << endl;
+    
+    int q = 0;
+    cout<< !q << endl;
+    
+    
+} 
