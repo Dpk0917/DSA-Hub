584ae4ffa047eb655babece3508e8f46c4320fef
DEEPAK <dpk0917@gmail.com>
2025-05-08 16:04:41 +0530

    Adding tuf-array-15.cpp

---
+++ tuf-array-15.cpp
@@ -0,0 +1,23 @@
+//RUN TIME ERROR
+
+class Solution {
+    public:
+        vector<int> leaders(vector<int>& nums) {
+          int n=nums.size();
+          //for empty nums
+          if (n == 0) return {};
+          vector<int>ans;
+    
+          ans.push_back(nums[n-1]);
+          int leader=nums[n-1];
+    
+          for(int i=n-2;i>=0;i++){
+            if(leader<nums[i]){
+                ans.push_back(nums[i]);
+                leader=nums[i];
+            }
+          }
+          reverse(ans.begin(), ans.end());
+          return ans;
+        }
+    };
