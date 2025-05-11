06b58c08fb5f340511033730e615f0ec4c5336e9
DEEPAK <dpk0917@gmail.com>
2025-05-11 13:30:31 +0530

    Adding tuf-array-18.cpp

---
+++ tuf-array-18.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:
+    int stockBuySell(vector<int> arr, int n){
+        int mini=arr[0];
+        int cost=0;
+        int profit=0;
+        for(int i=1;i<n;i++){
+            cost=arr[i]-mini;
+            profit=max(profit,cost);
+            mini=min(mini,arr[i]);
+        }
+        return profit;
+    }
+};
+
