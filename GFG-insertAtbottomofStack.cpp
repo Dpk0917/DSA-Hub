8355ba1281b434ff5fc09dfa12c42f09497c14c2
DEEPAK <dpk0917@gmail.com>
2025-06-05 12:11:48 +0530

    Adding GFG-insertAtbottomofStack.cpp

---
+++ GFG-insertAtbottomofStack.cpp
@@ -0,0 +1,26 @@
+// User function Template for C++
+
+class Solution {
+    private:
+    void solve(stack<int>&s, int x){
+
+    if(s.empty()){
+        s.push(x);
+        return;
+    }
+
+   int num=s.top();
+   s.pop();
+
+   solve(s,x);
+
+   s.push(num);
+}
+
+  public:
+    stack<int> insertAtBottom(stack<int> st, int x) {
+        
+        solve(st,x);
+        return st;
+    }
+};
