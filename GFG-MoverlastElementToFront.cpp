0bd0a21326845e090233b312f8825c484bd83e2e
DEEPAK <dpk0917@gmail.com>
2025-06-07 00:22:30 +0530

    Adding GFG-MoverlastElementToFront.cpp

---
+++ GFG-MoverlastElementToFront.cpp
@@ -0,0 +1,26 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    Node *moveToFront(Node *head) {
+        // code here
+        
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        Node*curr=head;
+        Node*prev=NULL;
+        
+        while(curr->next!=NULL){
+            prev=curr;
+            curr=curr->next;
+         }
+         prev->next=NULL;
+         curr->next=head;
+         
+         head=curr;
+         
+        return head;
+        
+    }
+};
