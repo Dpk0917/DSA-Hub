26c104f8ccc8f143ff6cc78c994dae0185b3dcfe
DEEPAK <dpk0917@gmail.com>
2025-07-22 14:57:45 +0530

    Adding GFG-FirstAndLastOccurrence.cpp

---
+++ GFG-FirstAndLastOccurrence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> find(vector<int>& arr, int x) {
+        // code here
+        int first=-1;
+        int last=-1;
+        
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==x){
+                if(first==-1){
+                    first=i;
+                    last=i;
+                }
+                last=i;
+            }
+        }
+        
+        return {first,last};
+    }
+};
