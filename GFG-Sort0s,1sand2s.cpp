892dedf92c1b649f0cff85a8fcf8b5d513f7b022
DEEPAK <dpk0917@gmail.com>
2025-07-23 11:05:15 +0530

    Adding GFG-Sort0s,1sand2s.cpp

---
+++ GFG-Sort0s,1sand2s.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        
+        sort(arr.begin(),arr.end());
+        
+        // for(int i=0;i<arr.size();i++){
+        //     for(int j=i+1;j<arr.size()-1;j++){
+        //         if(arr[i]>=arr[j]){
+        //             swap(arr[i],arr[j]);
+        //         }
+        //     }
+            
+        // }
+        
+    }
+};
