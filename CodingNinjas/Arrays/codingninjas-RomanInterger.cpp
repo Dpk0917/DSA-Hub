f9c7eced79ba6bbb7b61cc09d66d6dcf5374a8b5
DEEPAK <dpk0917@gmail.com>
2025-08-04 13:38:18 +0530

    codingninjas-RomanInterger.cpp

---
+++ CodingNinjas/Arrays/codingninjas-RomanInterger.cpp
@@ -0,0 +1,16 @@
+int romanToInt(string s) {
+    // Write your code here
+    unordered_map<char,int>Roman={
+            {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
+        };
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            if(i+1<s.length() && Roman[s[i]]<Roman[s[i+1]]){
+                ans-=Roman[s[i]];
+            }else{
+                ans+=Roman[s[i]];
+            }
+        }
+
+        return ans;
+}
