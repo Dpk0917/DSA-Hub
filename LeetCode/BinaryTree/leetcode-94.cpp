4b96eff8aae9194046f24c3da7b2ad5887842a6c
DEEPAK <dpk0917@gmail.com>
2025-08-05 09:31:24 +0530

    leetcode-94.cpp

---
+++ LeetCode/BinaryTree/leetcode-94.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(root==NULL){
+            return ans;
+        }
+        
+        TreeNode*curr=root;
+        TreeNode*pre=NULL;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                pre=curr->left;
+                while(pre->right!=NULL && pre->right!=curr){
+                    pre=pre->right;
+                }
+                
+                if(pre->right==NULL){
+                    pre->right=curr;
+                    curr=curr->left;
+                }else{
+                    pre->right=NULL;
+                    ans.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
