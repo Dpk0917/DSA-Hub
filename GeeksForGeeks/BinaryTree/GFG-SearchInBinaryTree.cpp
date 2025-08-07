31055a073708c3bf72c3aff9cb09fffcfe8c938e
DEEPAK <dpk0917@gmail.com>
2025-08-07 10:40:14 +0530

    GFG-SearchInBinaryTree.cpp

---
+++ GeeksForGeeks/BinaryTree/GFG-SearchInBinaryTree.cpp
@@ -0,0 +1,17 @@
+// Function to search a node in BST.
+bool search(Node* root, int x) {
+    // Your code here
+    if(root==NULL){
+        return false;
+    }
+
+    if(root->data==x){
+        return true;
+    }
+
+    if(root->data > x){
+        return search(root->left,x);
+    }else{
+        return search(root->right,x);
+    }
+}
