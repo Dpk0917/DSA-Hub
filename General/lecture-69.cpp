b00e050d956f536a71d6ae8ac4cafdce48722bc3
DEEPAK <dpk0917@gmail.com>
2025-08-09 22:43:11 +0530

    Lecture-69

--- General/lecture-69.cpp
+++ General/lecture-69.cpp
@@ -40,6 +40,40 @@ void levelOrderTraversal(Node* root) {
     }
 }
 
+Node* deleteNode(Node* root, int d) {
+    // base case
+    if (root == NULL) return NULL;
+
+    if(root->data == d) {
+        // case 1: no child
+        if (root->left == NULL && root->right == NULL) {
+            delete root;
+            return NULL;
+        }
+        // case 2: one child
+        if (root->left != NULL && root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        if (root->left == NULL && root->right != NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        // case 3: two children
+        Node* replace = root->right; // find the inorder successor
+        while (replace->left != NULL) {
+            replace = replace->left;
+        }
+        root->data = replace->data; // copy the value
+        root->right = deleteNode(root->right, replace->data); // delete the inorder successor
+    } else if (d < root->data) {
+        root->left = deleteNode(root->left, d);
+    } else {
+        root->right = deleteNode(root->right, d);
+    }
+}
 
 Node* insertIntoBST(Node* root, int d) {
     // base case
