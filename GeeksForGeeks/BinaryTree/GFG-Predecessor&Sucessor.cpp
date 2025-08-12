58b22298f87381e259856fd11687e72f142b7026
DEEPAK <dpk0917@gmail.com>
2025-08-12 10:21:20 +0530

    GFG-Predecessor&Sucessor.cpp Fic

--- GeeksForGeeks/BinaryTree/GFG-Predecessor&Sucessor.cpp
+++ GeeksForGeeks/BinaryTree/GFG-Predecessor&Sucessor.cpp
@@ -1,62 +1,38 @@
-geeksforgeeks
-Courses
-Tutorials
-Practice
-Jobs
-D
+class Solution {
+  public:
+    vector<Node*> findPreSuc(Node* root, int key) {
+        Node* pred = NULL;
+        Node* succ = NULL;
+        Node* temp = root;
 
-Search...
+        // Step 1: Find the key and store potential predecessors/successors
+        while (temp && temp->data != key) {
+            if (temp->data > key) {
+                succ = temp;   // this node could be successor
+                temp = temp->left;
+            } else {
+                pred = temp;   // this node could be predecessor
+                temp = temp->right;
+            }
+        }
 
+        if (!temp) return {pred, succ}; // key not found
 
+        // Step 2: Max value in left subtree -> predecessor
+        Node* leftTree = temp->left;
+        while (leftTree) {
+            pred = leftTree;
+            leftTree = leftTree->right;
+        }
 
-Upgrade to Premium for Doubt Support across all your problems and courses.
+        // Step 3: Min value in right subtree -> successor
+        Node* rightTree = temp->right;
+        while (rightTree) {
+            succ = rightTree;
+            rightTree = rightTree->left;
+        }
 
-Explore Premium
-Redirection Icon
-Predecessor and Successor
-Difficulty: MediumAccuracy: 47.36%Submissions: 162K+Points: 4
-You are given root node of the BST and an integer key. You need to find the in-order successor and predecessor of the given key. If either predecessor or successor is not found, then set it to NULL.
-
-Note:- In an inorder traversal the number just smaller than the target is the predecessor and the number just greater than the target is the successor. 
-
-Examples :
-
-Input: root[] = [8, 1, 9, N, 4, N, 10, 3, N, N, N], key = 8
-
-Output: 4 9
-Explanation: In the given BST the inorder predecessor of 8 is 4 and inorder successor of 8 is 9.
-Input: root[] = [10, 2, 11, 1, 5, N, N, N, N, 3, 6, N, 4, N, N], key = 11
-
-Output: 10 -1
-Explanation: In given BST, the inorder predecessor of 11 is 10 whereas it does not have any inorder successor.
-Input: root[] = [2, 1, 3], key = 3
-
-Output: 2 -1
-Explanation: In given BST, the inorder predecessor of 3 is 2 whereas it does not have any inorder successor.
-Constraints: 
-1 <= no. of nodes <= 105
-1 <= node->data <= 106
-1 <= key <= 106
-
-Expected Complexities
-Company Tags
-Ola Cabs
-Topic Tags
-Related Interview Experiences
-Related Articles
-If you are facing any issue on this page. Please let us know.
-Output Window
-Request Queued
-Ready for evaluation 
-
-C++ (12)
-
-
-
+        return {pred, succ};
+    }
 };
-
-
-
- 
-
-Custom Input
+#include <bits/stdc++.h>
