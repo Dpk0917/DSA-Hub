2958a48168122a45dfc9e45feb9afdb10836fb27
DEEPAK <dpk0917@gmail.com>
2025-08-25 12:56:44 +0530

    leetcode-1493.cpp

---
+++ LeetCode/BinaryTree/leetcode-1493.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int left=0,maxi=0,zero=0;
+
+        for(int i=0;i<nums.size();i++){
+
+            if(nums[i]==0){
+                zero++;
+
+            }
+
+            while(zero>1){
+                if(nums[left]==0){
+                    zero--;
+                }
+
+                left++;
+            }
+
+            maxi=max(maxi,i-left+1);
+        }
+
+        return maxi-1;
+
+
+    }
+};
