10d86122d80a28d07cc4ac6da03e4b0ffb892698
DEEPAK <dpk0917@gmail.com>
2025-09-08 13:25:52 +0530

    leetcode-1317

---
+++ LeetCode/Math/leetcode-1317.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    // helper to check if a number has '0' in it
+    bool isNoZero(int x) {
+        while (x > 0) {
+            if (x % 10 == 0) return false;
+            x /= 10;
+        }
+        return true;
+    }
+
+    vector<int> getNoZeroIntegers(int n) {
+        for (int a = 1; a < n; a++) {
+            int b = n - a;
+            if (isNoZero(a) && isNoZero(b)) {
+                return {a, b};
+            }
+        }
+        return {}; // should never reach here because a solution always exists
+    }
+};
