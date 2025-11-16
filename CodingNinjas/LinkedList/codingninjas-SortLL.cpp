001f3f33dab34fc8d06b27a63b1240e9c2c6185f
Deepak Singh <dpk0917@gmail.com>
2025-11-16 15:52:55 +0530

    codingninjas-SortLL.cpp

---
+++ CodingNinjas/LinkedList/codingninjas-SortLL.cpp
@@ -0,0 +1,53 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+
+Node* sortList(Node* head){
+    // Write your code here.
+    if(head==NULL && head->next==NULL){
+            return head;
+        }
+        
+        Node*curr=head;
+        vector<int>ans;
+        
+        while(curr!=NULL){
+            ans.push_back(curr->data);
+            curr=curr->next;
+        }
+        
+        sort(ans.begin(),ans.end());
+        
+        int i=0;
+        curr=head;
+        
+        while(curr!=NULL){
+            curr->data=ans[i];
+            i++;
+            curr=curr->next;
+        }
+        
+        return head;
+}
