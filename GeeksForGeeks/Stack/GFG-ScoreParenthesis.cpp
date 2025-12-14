86be245a3a45876d4efc4cd76ed42dad9a1af14b
Deepak Singh <dpk0917@gmail.com>
2025-12-14 13:50:44 +0530

    GFG-ScoreParenthesis.cpp

---
+++ GeeksForGeeks/Stack/GFG-ScoreParenthesis.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int scoreOfParentheses(string& s) {
+        // code here
+        stack<char>star;
+        int ans=0;
+
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+
+            if(ch=='('){
+                star.push(ch);
+            }else{
+                star.pop();
+
+                if(s[i-1]=='(') {
+                    ans+=pow(2,star.size());
+                }
+            }
+
+        }
+
+        return ans;
+    }
+};
