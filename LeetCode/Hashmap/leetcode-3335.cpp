dfccd9a5eefe748488262325fe06eb4423c39d03
Deepak Singh <dpk0917@gmail.com>
2026-05-06 23:59:25 +0530

    Adding fixing leetcode-3335.cpp

--- LeetCode/Hashmap/leetcode-3335.cpp
+++ LeetCode/Hashmap/leetcode-3335.cpp
@@ -1,32 +1,41 @@
 class Solution {
 public:
-    static const int M=1e9+7;
+    int lengthAfterTransformations(string s, int t) {
+        const int M = 1e9+7;
 
-    int lengthAfterTransformations(string s,int t) {
-        vector<long long> cnt(26,0);
+        int n=s.length();
 
-        for(char ch:s) {
-            cnt[ch-'a']++;
+        vector<int>map(26,0);
+
+        for(char &ch:s){
+            map[ch-'a']++;
         }
 
-        for(int step=0;step<t;step++) {
-            vector<long long> temp(26,0);
+        for(int i=1;i<=t;i++){
+            vector<int>temp(26,0);
 
-            for(int i=0;i<25;i++) {
-                temp[i+1]=(temp[i+1]+cnt[i])%M;
+            for(int j=0;j<26;j++){
+                char ch=j+'a';
+                int freq=map[j];
+
+                if(ch!='z'){
+                    temp[(ch+1)-'a']=(temp[(ch+1)-'a']+freq)%M;
+
+                }else{
+                    temp['a'-'a']=(temp['a'-'a']+freq)%M;
+                    temp['b'-'a']=(temp['b'-'a']+freq)%M;
+
+                }
             }
 
-            temp[0]=(temp[0]+cnt[25])%M;
-            temp[1]=(temp[1]+cnt[25])%M;
-
-            cnt=move(temp);
+            map=move(temp);
         }
 
-        long long ans=0;
-
-        for(int i=0;i<26;i++) {
-            ans=(ans+cnt[i])%M;
+        int ans=0;
+        for(int i=0;i<26;i++){
+            ans=(ans+map[i])%M;
         }
+
 
         return ans;
     }
