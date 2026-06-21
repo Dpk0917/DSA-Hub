f2184c4dd4a4a5370f4ce661d4991051800f4e6e
Deepak Singh <dpk0917@gmail.com>
2026-06-21 21:35:22 +0530

    Fixing leetcode-1833.cpp

---
+++ LeetCode/Arrays/leetcode-1833.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+        int ans=0;
+        for(int i=0;i<costs.size();i++){
+            if(coins<costs[i]){
+                return ans;
+
+            }
+
+            coins=coins-costs[i];
+            ans++;
+
+        }
+
+        return ans;
+    }
+};
